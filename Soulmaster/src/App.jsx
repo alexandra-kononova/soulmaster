982f17ef468157d793cb3cd4da73fd6a93c3ac90
Alexandra Kononova <akononova93@gmail.com>
2024-10-21 15:48:39 -0400

    Update project sctructure

---
+++ Soulmaster/src/App.jsx
@@ -0,0 +1,23 @@
+// import "./App.scss";
+// import Header from "./components/Header/Header";
+// import Home from "./pages/Home/Home";
+// import VideoUpload from "./pages/VideoUpload/VideoUpload";
+// import NotFound from "./pages/NotFound/NotFound";
+// import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Header />
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/video/:videoId" element={<Home />} />
+//         <Route path="/upload" element={<VideoUpload />} />
+//         <Route path="/not-found" element={<NotFound />} />
+//         <Route path="/*" element={<Navigate to="/not-found" />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
